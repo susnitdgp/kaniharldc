99af4bfd7c9afa2f7e4cca273a04d47b7fb30953
Susanta Goswami <susnitdgp@gmail.com>
2017-07-06 19:55:26 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/python
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+	def get(self):
+		self.response.write("Hello test Message")
+		
+		
+app=webapp2.WSGIApplication([
+	("/",MainHandler)
+],debug=True)
+			
+	
